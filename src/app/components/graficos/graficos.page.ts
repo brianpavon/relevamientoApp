47e00c91c8b01401c5e958eadd0160106f9c1cd2
53911453+brianpavon@users.noreply.github.com <53911453+brianpavon@users.noreply.github.com>
2023-06-01 16:45:24 -0300

    cambios de la corrección del recu

--- src/app/components/graficos/graficos.page.ts
+++ src/app/components/graficos/graficos.page.ts
@@ -134,45 +134,45 @@ export class GraficosPage implements OnInit {
 
   graficoBarras() {
 
-    // this.barChart = new Chart(this.barCanvas.nativeElement, {
-    //   type: 'bar',
-    //   data: {
-    //     labels: this.labelsFeos,
-    //     datasets: [{
-    //       label: 'Cantidad de me gusta',
-    //       data: this.cantLikesFeos,
-    //       backgroundColor: [
-    //         'rgba(255, 99, 132, 0.2)',
-    //         'rgba(54, 162, 235, 0.2)',
-    //         'rgba(255, 206, 86, 0.2)',
-    //         'rgba(75, 192, 192, 0.2)',
-    //         'rgba(153, 102, 255, 0.2)',
-    //         'rgba(255, 159, 64, 0.2)'
-    //       ],
-    //       borderColor: [
-    //         'rgba(255,99,132,1)',
-    //         'rgba(54, 162, 235, 1)',
-    //         'rgba(255, 206, 86, 1)',
-    //         'rgba(75, 192, 192, 1)',
-    //         'rgba(153, 102, 255, 1)',
-    //         'rgba(255, 159, 64, 1)'
-    //       ],
-    //       borderWidth: 1
-    //     }]
-    //   },
-    //   options: {
-    //     scales: {
-    //       y: {
-    //           beginAtZero: true,
-    //           ticks:{
-    //             stepSize:0.5
-    //           }
-    //       }
-    //     }
-    //   }
-    // });
+    this.barChart = new Chart(this.barCanvas.nativeElement, {
+      type: 'bar',
+      data: {
+        labels: this.labelsFeos,
+        datasets: [{
+          label: 'Cantidad de me gusta',
+          data: this.cantLikesFeos,
+          backgroundColor: [
+            'rgba(255, 99, 132, 0.2)',
+            'rgba(54, 162, 235, 0.2)',
+            'rgba(255, 206, 86, 0.2)',
+            'rgba(75, 192, 192, 0.2)',
+            'rgba(153, 102, 255, 0.2)',
+            'rgba(255, 159, 64, 0.2)'
+          ],
+          borderColor: [
+            'rgba(255,99,132,1)',
+            'rgba(54, 162, 235, 1)',
+            'rgba(255, 206, 86, 1)',
+            'rgba(75, 192, 192, 1)',
+            'rgba(153, 102, 255, 1)',
+            'rgba(255, 159, 64, 1)'
+          ],
+          borderWidth: 1
+        }]
+      },
+      options: {
+        scales: {
+          y: {
+              beginAtZero: true,
+              ticks:{
+                stepSize:0.5
+              }
+          }
+        }
+      }
+    });
 
-    // this.barChart.destroy();
+    this.barChart.destroy();
     this.barChart = new Chart(this.barCanvas.nativeElement, {
       type: 'bar',
       data: {
